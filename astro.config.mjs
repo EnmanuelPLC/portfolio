f71bc5e468bdc77bbfd77800a880fbc12d4f77bc
Enmanuel Ponce <enmanuelponcedeleon@gmail.com>
2024-03-03 17:48:48 -0500

    update colors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://EnmanuelPLC.github.io',
-    base: '/portfolio',
+  site: 'https://EnmanuelPLC.github.io',
+  base: '/portfolio',
+  integrations: [tailwind()]
 });
